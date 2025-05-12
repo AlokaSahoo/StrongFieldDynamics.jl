7330256e24b89e324e5e1f79c75230a72fdc9c4b
Documenter.jl <documenter@juliadocs.github.io>
2025-05-12 20:52:36 +0000

    build based on 8b0241b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#StrongFieldDynamics","page":"Home","title":"StrongFieldDynamics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StrongFieldDynamics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"A Julia package for studying intense-laser atom interactions. Julia is fast.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
