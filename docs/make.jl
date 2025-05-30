bbe8a1b28746e9580d59ffc8dd6d661df30ff78b
sahoo-ak <aloka.ku.sahoo@gmail.com>
2025-05-31 00:59:13 +0200

    revert documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,5 @@
 using StrongFieldDynamics
 using Documenter
-using DocumenterVitepress
 
 DocMeta.setdocmeta!(StrongFieldDynamics, :DocTestSetup, :(using StrongFieldDynamics); recursive=true)
 
@@ -8,18 +7,17 @@ makedocs(;
     modules=[StrongFieldDynamics],
     authors="Aloka Kumar Sahoo <aloka_s@ph.iitr.ac.in>",
     sitename="StrongFieldDynamics.jl",
-    format = DocumenterVitepress.MarkdownVitepress(
-        repo="https://AlokaSahoo.github.io/StrongFieldDynamics.jl",
+    format=Documenter.HTML(;
+        canonical="https://AlokaSahoo.github.io/StrongFieldDynamics.jl",
+        edit_link="main",
+        assets=String[],
     ),
     pages=[
         "Home" => "index.md",
     ],
 )
 
-DocumenterVitepress.deploydocs(;
-    repo = "github.com/AlokaSahoo/StrongFieldDynamics.jl",
-    target = "build",
-    devbranch = "main",
-    branch = "gh-pages",
-    push_preview = true,
+deploydocs(;
+    repo="github.com/AlokaSahoo/StrongFieldDynamics.jl",
+    devbranch="main",
 )
